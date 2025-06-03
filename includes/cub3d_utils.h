49ecbd67910bc1ef489bf023317547e6ae68d74b
Daruuu <thedaruny16@gmail.com>
2025-06-03 13:53:25 +0200

    udpate cub3d_utils

--- includes/cub3d_utils.h
+++ includes/cub3d_utils.h
@@ -3,59 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d_utils.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasalaza <dasalaza@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: dasalaza <dasalaza@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:06:02 by dasalaza          #+#    #+#             */
-/*   Updated: 2025/05/28 17:06:11 by dasalaza         ###   ########.fr       */
+/*   Updated: 2025/06/03 12:12:01 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_UTILS_H
 # define CUB3D_UTILS_H
 
-/*--------------------Error and free------------------*/
-# define ERROR_INVALID_ARGS "Error: Incorrect arguments.\n"
-# define INVALID_LEN_FILE_MAP "Error: Map filename too short.\n"
-# define INVALID_MAP_EXTENSION "Error: Extension invalid, expected '.cub'\n"
-# define ERROR_CHARS_IN_FILENAME "Error: filename.cub contains invalid chars.\n"
-# define ERROR_OPEN_FILE_MAP "Error: Map file could not be opened.\n"
-# define ERROR_EMPTY_FILE_MAP "Error: File map is empty.\n"
+/*-------------------- Memory & Allocation Errors --------------------*/
+# define ERROR_MEMORY_ALLOCATION       "Error: Memory allocation failed\n"
+# define ERROR_MAP_ALLOC_FAILED        "Error: Failed to allocate memory for map\n"
 
-# define INVALID_LATERALS_MAP "Error: Invalid map dimensions\n"
-# define INVALID_TOP_BOTTOM_MAP "Error: Invalid border top or bottom\n"
+/*-------------------- Argument & File Errors ------------------------*/
+# define ERROR_INVALID_ARGS            "Error: Incorrect arguments.\n"
+# define INVALID_LEN_FILE_MAP          "Error: Map filename too short.\n"
+# define INVALID_MAP_EXTENSION         "Error: Extension invalid, expected '.cub'\n"
+# define ERROR_CHARS_IN_FILENAME       "Error: filename.cub contains invalid chars.\n"
+# define ERROR_OPEN_FILE_MAP           "Error: Map file could not be opened.\n"
+# define ERROR_EMPTY_FILE_MAP          "Error: File map is empty.\n"
+# define ERR_INVALID_MAP_EXTRACT       "Error: Invalid map. Can't extract from .cub\n"
 
-# define ERROR_INVALID_MAP_ROWS "Error: Invalid rows\n"
-# define ERROR_INVALID_MAP_COLS "Error: Invalid columns\n"
+/*-------------------- Map Structure & Dimensions --------------------*/
+# define INVALID_LATERALS_MAP          "Error: Invalid map dimensions\n"
+# define INVALID_TOP_BOTTOM_MAP        "Error: Invalid border top or bottom\n"
+# define ERROR_INVALID_MAP_ROWS        "Error: Invalid rows\n"
+# define ERROR_INVALID_MAP_COLS        "Error: Invalid columns\n"
+# define ERROR_TABULATION_IN_MAP       "Error: A tabulation exists in map.\n"
 
-# define ERROR_MEMORY_ALLOCATION "Error: Memory allocation failed\n"
+/*-------------------- Map Wall & Border Validation ------------------*/
+# define ERROR_WALLS_IN_MAP            "Error: Invalid WALLS in map.\n"
 
-# define ERROR_WALLS_IN_MAP "Error: Invalid WALLS in map.\n"
-# define ERROR_PLAYER_NOT_EXIST "Error: Player position NOT exists in map\n"
-# define ERROR_COUNT_PLAYER_INVALID "Error: More than 1 PLAYER in map.\n"
+/*-------------------- Player Validation -----------------------------*/
+# define ERROR_PLAYER_NOT_EXIST        "Error: Player position NOT exists in map\n"
+# define ERROR_COUNT_PLAYER_INVALID    "Error: More than 1 PLAYER in map.\n"
 
-# define ERROR_ITEMS_IN_MAP "Error: Invalid ITEM in map.\n"
+/*-------------------- Map Items & Characters ------------------------*/
+# define ERROR_ITEMS_IN_MAP            "Error: Invalid ITEM in map.\n"
 
-# define ERROR_INVALID_NORTH_TEXTURE "Error: Invalid texture NORTH\n"
-# define ERROR_INVALID_SOUTH_TEXTURE "Error: Invalid texture SOUTH\n"
-# define ERROR_INVALID_WEST_TEXTURE "Error: Invalid texture WEST\n"
-# define ERROR_INVALID_EAST_TEXTURE "Error: Invalid texture EAST\n"
+/*-------------------- Texture & Orientation Errors ------------------*/
+# define ERROR_INVALID_NORTH_TEXTURE   "Error: Invalid texture NORTH\n"
+# define ERROR_INVALID_SOUTH_TEXTURE   "Error: Invalid texture SOUTH\n"
+# define ERROR_INVALID_WEST_TEXTURE    "Error: Invalid texture WEST\n"
+# define ERROR_INVALID_EAST_TEXTURE    "Error: Invalid texture EAST\n"
+# define ERROR_ITEM_NORTH              "Error: Invalid items NORTH.\n"
+# define ERROR_ITEM_SOUTH              "Error: Invalid items SOUTH.\n"
+# define ERROR_ITEM_WEST               "Error: Invalid items WEST.\n"
+# define ERROR_ITEM_EAST               "Error: Invalid items EAST.\n"
 
-# define ERROR_INVALID_FLOOR_TEXTURE "Error: Invalid format of FLOOR\n"
+/*-------------------- Floor & Ceiling Color Errors ------------------*/
+# define ERROR_INVALID_FLOOR_TEXTURE   "Error: Invalid format of FLOOR\n"
 # define ERROR_INVALID_CEILING_TEXTURE "Error: Invalid format of CEIL\n"
 
-# define ERROR_ITEM_NORTH "Error: Invalid items NORTH.\n"
-# define ERROR_ITEM_SOUTH "Error: Invalid items SOUTH.\n"
-# define ERROR_ITEM_WEST "Error: Invalid items WEST.\n"
-# define ERROR_ITEM_EAST "Error: Invalid items EAST.\n"
-
-# define ERROR_TABULATION_IN_MAP "Error: A tabulation exist in map.\n"
-
-# define ERR_INVALID_MAP_EXTRACT "Error: Invalid map. Can't extract from .cub\n"
-# define ERROR_MAP_ALLOC_FAILED "Error: Failed to allocate memory for map"
-
-/*--------------------Error and exit------------------*/
-
-/*--------------------Error items in map------------------*/
 
 /*-------------------- ITEMS ASSIGN ------------------*/
 # define WALL '1'
